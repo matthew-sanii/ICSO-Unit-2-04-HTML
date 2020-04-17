96d3bcda0f493b0965e7a7d2f30160a53fcf8d4c
matthew.sanii <matthew.sanii@mths.ca>
2020-04-17 18:40:58 +0000

    edited index.html

---
+++ Click.js
@@ -0,0 +1,9 @@
+// JavaScript File
+
+let divresult;
+let info=document.getElementById("diameter");
+function Click(){
+    divresult=(info.value);
+    divresult.innerHTML=info;
+    alert(divresult);
+}
